98e63918caf299a4ba311c108dbe6debb5a66725
FineDr <finesawa36@gmail.com>
2025-05-06 16:19:54 +0300

    add the backend changes to the codebase

--- api/views.py
+++ api/views.py
@@ -1,22 +1,28 @@
-# views.py
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Student, Subject
 from .serializers import StudentSerializer, SubjectSerializer
+import os
 
 class StudentsListView(APIView):
     def get(self, request):
         students = Student.objects.all()
         data = {"students": StudentSerializer(students, many=True).data}
-        return Response(data)
+        node_id = os.getenv('NODE_ID', 'Unknown')
+        response = Response(data)
+        response.headers['X-Node-ID'] = node_id
+        return response
 
     def post(self, request):
         serializer = StudentSerializer(data=request.data)
         if serializer.is_valid():
             student = serializer.save()
             data = {"student": StudentSerializer(student).data}
-            return Response(data, status=status.HTTP_201_CREATED)
+            node_id = os.getenv('NODE_ID', 'Unknown')
+            response = Response(data, status=status.HTTP_201_CREATED)
+            response.headers['X-Node-ID'] = node_id
+            return response
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -24,12 +30,18 @@ class SubjectsListView(APIView):
     def get(self, request):
         subjects = Subject.objects.all()
         data = {"subjects": SubjectSerializer(subjects, many=True).data}
-        return Response(data)
+        node_id = os.getenv('NODE_ID', 'Unknown')
+        response = Response(data)
+        response.headers['X-Node-ID'] = node_id
+        return response
 
     def post(self, request):
         serializer = SubjectSerializer(data=request.data)
         if serializer.is_valid():
             subject = serializer.save()
             data = {"subject": SubjectSerializer(subject).data}
-            return Response(data, status=status.HTTP_201_CREATED)
+            node_id = os.getenv('NODE_ID', 'Unknown')
+            response = Response(data, status=status.HTTP_201_CREATED)
+            response.headers['X-Node-ID'] = node_id
+            return response
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
